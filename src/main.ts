ef5293bb2fbfdae3882c2b9b3cc79bb95eabb932
Muhammad Umair <mianumair1958@gmail.com>
2024-11-19 00:25:29 +0500

    made some changes

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,31 @@ import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist:true,
-    transform: true,
-  }
-  ))
-  app.use(cors({
-    origin: 'https://gen-max.vercel.app', // Replace with your allowed origin
-  }));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  const allowedOrigins = [
+    'https://gen-max.vercel.app', 
+    'https://localhost:4200',
+  ];
+
+  app.use(
+    cors({
+      origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
-//
